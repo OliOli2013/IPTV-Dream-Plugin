4faf6a29880d012bd603d81eb36f94efa38a5841
Pawel Pawelek <pawelzarzycki61@gmail.com>
2025-10-13 14:43:45 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
-# Skrypt instalacyjny dla wtyczki IPTV Dream
+# Skrypt instalacyjny dla wtyczki IPTV Dream v2.1
 
-# Ustawienie ścieżki do folderu z wtyczkami Enigma2
+# Ustawienie ścieżek
 PLUGIN_PATH="/usr/lib/enigma2/python/Plugins/Extensions/IPTVDream"
+TOOLS_PATH="${PLUGIN_PATH}/tools"
 
 echo ">>> Usuwanie poprzedniej wersji wtyczki (jeśli istnieje)..."
 rm -rf ${PLUGIN_PATH}
 
-echo ">>> Tworzenie folderu dla nowej wersji wtyczki..."
+echo ">>> Tworzenie folderów dla nowej wersji wtyczki..."
 mkdir -p ${PLUGIN_PATH}
+mkdir -p ${TOOLS_PATH}
 
-echo ">>> Pobieranie plików wtyczki z GitHub..."
+echo ">>> Pobieranie głównych plików wtyczki z GitHub..."
 wget -q "https://raw.githubusercontent.com/OliOli2013/IPTV-Dream-Plugin/main/dream.py" -P ${PLUGIN_PATH}
 wget -q "https://raw.githubusercontent.com/OliOli2013/IPTV-Dream-Plugin/main/export.py" -P ${PLUGIN_PATH}
 wget -q "https://raw.githubusercontent.com/OliOli2013/IPTV-Dream-Plugin/main/file_pick.py" -P ${PLUGIN_PATH}
@@ -20,13 +22,18 @@ wget -q "https://raw.githubusercontent.com/OliOli2013/IPTV-Dream-Plugin/main/plu
 wget -q "https://raw.githubusercontent.com/OliOli2013/IPTV-Dream-Plugin/main/plugin.py" -P ${PLUGIN_PATH}
 wget -q "https://raw.githubusercontent.com/OliOli2013/IPTV-Dream-Plugin/main/vkb_input.py" -P ${PLUGIN_PATH}
 
+echo ">>> Pobieranie plików z katalogu /tools..."
+wget -q "https://raw.githubusercontent.com/OliOli2013/IPTV-Dream-Plugin/main/tools/lang.py" -P ${TOOLS_PATH}
+wget -q "https://raw.githubusercontent.com/OliOli2013/IPTV-Dream-Plugin/main/tools/mac_portal.py" -P ${TOOLS_PATH}
+wget -q "https://raw.githubusercontent.com/OliOli2013/IPTV-Dream-Plugin/main/tools/updater.py" -P ${TOOLS_PATH}
+
 echo ">>> Ustawianie odpowiednich uprawnień dla plików..."
 chmod -R 755 ${PLUGIN_PATH}
 
 echo "*****************************************************"
 echo "** **"
-echo "** Instalacja zakończona pomyślnie!           **"
-echo "** Proszę zrestartować Enigma2, aby zmiany      **"
+echo "** Aktualizacja do v2.1 zakończona!           **"
+echo "** Proszę zrestartować Enigma2, aby zmiany     **"
 echo "** zostały wprowadzone.                 **"
 echo "** **"
 echo "*****************************************************"
