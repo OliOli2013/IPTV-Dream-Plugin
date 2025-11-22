956cbc29ec3c172d22c50afe4e55d24b3176f460
Pawel Pawelek <pawelzarzycki61@gmail.com>
2025-11-22 01:43:17 +0100

    Update epg_picon.py

--- tools/epg_picon.py
+++ tools/epg_picon.py
@@ -1,47 +1,51 @@
 # -*- coding: utf-8 -*-
 import os
 
-# Pewny link do polskiego EPG
-EPG_URL = "http://epg.ovh/pl.xml.gz"
+EPG_SOURCES = [
+    # POLSKA
+    {"url": "http://epg.ovh/pl.xml.gz", "description": "IPTV Dream - Polska (OVH)"},
+    {"url": "https://iptv-epg.org/files/epg-pl.xml.gz", "description": "IPTV Dream - Polska (iptv-epg)"},
+    # UK (GB)
+    {"url": "https://iptv-epg.org/files/epg-gb.xml.gz", "description": "IPTV Dream - UK (Great Britain)"},
+    # USA
+    {"url": "https://iptv-epg.org/files/epg-us.xml.gz", "description": "IPTV Dream - USA"},
+    # AFRYKA
+    {"url": "https://iptv-epg.org/files/epg-ug.xml.gz", "description": "IPTV Dream - Uganda"},
+    {"url": "https://iptv-epg.org/files/epg-tz.xml.gz", "description": "IPTV Dream - Tanzania"},
+    {"url": "https://iptv-epg.org/files/epg-za.xml.gz", "description": "IPTV Dream - South Africa"},
+    # EUROPA
+    {"url": "https://iptv-epg.org/files/epg-de.xml.gz", "description": "IPTV Dream - Germany"},
+    {"url": "https://iptv-epg.org/files/epg-fr.xml.gz", "description": "IPTV Dream - France"},
+    # ŚWIAT
+    {"url": "http://epg.bevy.be/bevy.xml.gz", "description": "IPTV Dream - World Mix (Bevy)"}
+]
 
-# Ścieżki - ZMIANA NAZWY PLIKU, aby uniknąć konfliktów
 EPG_DIR      = "/etc/epgimport"
-# Nowa nazwa pliku źródła:
-SOURCE_FILE  = "/etc/epgimport/iptvdream_fix.sources.xml"
+SOURCE_FILE  = "/etc/epgimport/iptvdream.sources.xml"
 CHANNEL_FILE = "/etc/epgimport/iptvdream.channels.xml"
 
-def create_epg_import_source():
-    """Tworzy unikalny plik źródła dla EPG Import"""
+def install_epg_sources():
     try:
-        if not os.path.exists(EPG_DIR):
-            os.makedirs(EPG_DIR)
-
-        # Treść pliku źródła z UNIKALNĄ nazwą kategorii
-        source_content = f"""<?xml version="1.0" encoding="utf-8"?>
-<sources>
-    <sourcecat sourcecatname="IPTV Dream (NAPRAWIONE)">
-        <source type="gen_xmltv" channels="iptvdream.channels.xml">
-            <description>Polska (EPG OVH)</description>
-            <url>{EPG_URL}</url>
-        </source>
-    </sourcecat>
-</sources>
-"""
-        # Zapisujemy plik źródła
-        with open(SOURCE_FILE, "w") as f:
-            f.write(source_content)
-            
-        # Upewniamy się, że plik kanałów też istnieje
+        if not os.path.exists(EPG_DIR): os.makedirs(EPG_DIR)
+        content = '<?xml version="1.0" encoding="utf-8"?>\n<sources>\n'
+        content += '    <sourcecat sourcecatname="IPTV Dream EPG (MEGA)">\n'
+        for source in EPG_SOURCES:
+            content += f"""        <source type="gen_xmltv" channels="iptvdream.channels.xml">
+            <description>{source['description']}</description>
+            <url>{source['url']}</url>
+        </source>\n"""
+        content += '    </sourcecat>\n</sources>\n'
+        with open(SOURCE_FILE, "w") as f: f.write(content)
         if not os.path.exists(CHANNEL_FILE):
-            with open(CHANNEL_FILE, "w") as f:
-                f.write('<?xml version="1.0" encoding="utf-8"?>\n<channels>\n</channels>')
-                
+            with open(CHANNEL_FILE, "w") as f: f.write('<?xml version="1.0" encoding="utf-8"?>\n<channels>\n</channels>')
+        return True, "Źródła EPG zaktualizowane."
     except Exception as e:
-        print(f"[IPTVDream] Błąd zapisu: {e}")
+        return False, f"Błąd: {e}"
 
 def fetch_epg_for_playlist(pl):
-    """Uruchamiane przy pobieraniu listy"""
-    create_epg_import_source()
+    install_epg_sources()
 
 def download_picon_url(url, title):
     return ""
+    
+create_epg_import_source = install_epg_sources
